8ac1fa4eeb67473629998f06c0499bfa414926b8
spl-D3V <spedraza.desarrollo@gmail.com>
2019-04-19 20:53:20 +0200

    limpiando

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const device = require('express-device');
 const app = express();
-const d = require('device');
-const a = d();
 // Middleware
 app.use(device.capture());
 
 app.get('/', (req, res) =>{
-    res.send('Hola ' +'' );
-});
-
-Object.keys(a['parser']).forEach(e => {
-    console.log(e, ' -> ',a[e]);
+    console.log(req.headers);
+    res.send('Hola ' +req.device.type );
 });
 
 app.listen(3000, ()=> console.log('Server running'));
